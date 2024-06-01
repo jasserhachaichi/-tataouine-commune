cd44568deee381d29b7657456d7d13cabb692747
jasserhachaichi <jasser.hchaichi@enig.u-gabes.tn>
2024-06-01 15:42:00 +0100

    Fix text language, add image section home, fix heading section style, fix faq, fix login image

--- routes/homeroute.js
+++ routes/homeroute.js
@@ -2,31 +2,58 @@ const express = require("express");
 const router = express.Router();
 const followerModel = require('./../models/Follower');
 const Blog = require('./../models/Blog');
+const Image = require('./../models/Image');
+router.use(express.static("public"));
+router.use(express.static("views"));
+router.use(express.static("Attachments"));
+
+const sharp = require('sharp');
 
 
 router.get("/", async (req, res) => {
-            // Fetch the three most recent blog posts
-            const recentBlogs = await Blog.find().sort({ createdAt: -1 }).limit(3);
-            // Render the home page and pass the recentBlogs to the template
-            return res.render("home", { recentBlogs });
+  try {
+    // Fetch the three most recent blog posts
+    const recentBlogs = await Blog.find().sort({ createdAt: -1 }).limit(3);
+    // Fetch the five most recent images
+    const recentImages = await Image.find().sort({ createdAt: -1 }).limit(4);
+    // Resize each image to 400x400
+    const resizedImages = await Promise.all(recentImages.map(async (image) => {
+      const resizedImageBuffer = await sharp(image.path)
+        .resize(400, 400)
+        .toBuffer();
+      return {
+        ...image.toObject(),
+        resizedPath: `data:image/jpeg;base64,${resizedImageBuffer.toString('base64')}`
+      };
+    }));
+
+    const updatedImages = recentImages.map(image => ({
+      ...image.toObject(),
+      path: image.path.replace("attachments\\", "").replace(/\\/g, '/')
+    }));
+    return res.render("home", { recentBlogs, recentImages: updatedImages, resizedImages: resizedImages });
+  } catch (error) {
+    console.error(error);
+    return res.redirect("/404");
+  }
 });
 
 router.post("/subscribe", async (req, res) => {
   try {
-      const { email } = req.body;
-      const existingFollower = await followerModel.findOne({ email });
+    const { email } = req.body;
+    const existingFollower = await followerModel.findOne({ email });
 
-      if (existingFollower) {
-        console.log("ee");
-          return res.status(400)();
+    if (existingFollower) {
+      console.log("ee");
+      return res.status(400)();
 
-      }
-      const newFollower = new followerModel({ email });
-      await newFollower.save();
-      console.log(email);
-      res.send("Subscription successful!");
+    }
+    const newFollower = new followerModel({ email });
+    await newFollower.save();
+    console.log(email);
+    res.send("Subscription successful!");
   } catch (error) {
-      res.status(500)("Internal Server Error");
+    res.status(500)("Internal Server Error");
   }
 });
 
